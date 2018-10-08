f7d924e58ec1d7f61fdf0010df6820b0e2156ab8
Meakins, Alex <alex.meakins@ukaea.uk>
2018-10-08 17:52:07 +0100

    Updated equilibrium handler to refine the interface. Added map2d and map3d methods to simplify mapping profiles onto equlibrium.

--- cherab/tools/equilibrium/efit.pyx
+++ cherab/tools/equilibrium/efit.pyx
@@ -26,8 +26,11 @@ from cherab.core.math cimport Function2D, autowrap_function2d
 from cherab.core.math cimport VectorFunction2D
 from cherab.core.math cimport Interpolate1DCubic, Interpolate2DCubic
 from cherab.core.math cimport PolygonMask2D
+from cherab.core.math cimport IsoMapper2D, AxisymmetricMapper
+from cherab.core.math cimport Blend2D, Constant2D
 
 
+# todo: decide if a load and save routine that stores as json (for now)?
 cdef class EFITEquilibrium:
 
     """
@@ -37,17 +40,18 @@ cdef class EFITEquilibrium:
     equilibrium attributes, such as the magnetic flux (psi) and magnetic
     field.
 
+    Note: psin_to_r mapping only exists if the psi axis is monotonic.
+
     :param r: EFIT grid radius axis values (array).
     :param z: EFIT grid height axis values (array).
     :param psi_grid: EFIT psi grid values (array).
     :param float psi_axis: The psi value at the magnetic axis.
     :param float psi_lcfs: The psi value at the LCFS.
     :param Point2D magnetic_axis: The coordinates of the magnetic axis.
-    :param f_profile_psin: The normalised psi axis values for the current flux profile (array).
-    :param f_profile_magnitude: The magnitudes of the current flux profile (array).
+    :param f_profile: The current flux profile on psin (2xN array).
     :param float b_vacuum_radius: Vacuum B-field reference radius (in meters).
     :param float b_vacuum_magnitude: Vacuum B-Field magnitude at the reference radius.
-    :param lcfs_polygon: An Nx2 array of (x, y) vertices specifying the LCFS boundary.
+    :param lcfs_polygon: An 2xN array of [[x0, ...], [y0, ...]] vertices specifying the LCFS boundary.
     :param float time: The time stamp of the time-slice (in seconds).
     """
 
@@ -65,8 +69,8 @@ cdef class EFITEquilibrium:
         Function2D _dpsidr, _dpsidz
 
     def __init__(self, object r, object z, object psi_grid, double psi_axis, double psi_lcfs,
-                 Point2D magnetic_axis not None, object f_profile_psin, object f_profile_magnitude,
-                 double b_vacuum_radius, double b_vacuum_magnitude, object lcfs_polygon, double time):
+                 Point2D magnetic_axis not None, object f_profile, double b_vacuum_radius,
+                 double b_vacuum_magnitude, object lcfs_polygon, double time):
 
         self.time = time
 
@@ -74,8 +78,7 @@ cdef class EFITEquilibrium:
         r = np.array(r, dtype=np.float64)
         z = np.array(z, dtype=np.float64)
         psi = np.array(psi_grid, dtype=np.float64)
-        f_profile_psin = np.array(f_profile_psin, dtype=np.float64)
-        f_profile_magnitude = np.array(f_profile_magnitude, dtype=np.float64)
+        f_profile = np.array(f_profile, dtype=np.float64)
 
         # interpolate poloidal flux grid data
         self.psi = Interpolate2DCubic(r, z, psi_grid)
@@ -89,7 +92,7 @@ cdef class EFITEquilibrium:
         self.magnetic_axis = magnetic_axis
         self._b_vacuum_magnitude = b_vacuum_magnitude
         self._b_vacuum_radius = b_vacuum_radius
-        self._f_profile = Interpolate1DCubic(f_profile_psin, f_profile_magnitude)
+        self._f_profile = Interpolate1DCubic(f_profile[0, :], f_profile[1, :])
 
         # EFIT lcfs polygon
         self.inside_lcfs = EFITLCFSMask(lcfs_polygon, self.psi_normalised)
@@ -105,9 +108,9 @@ cdef class EFITEquilibrium:
     cpdef tuple _calculate_differentials(self, np.ndarray r, np.ndarray z, np.ndarray psi_grid):
 
         # calculate differentials
-        dpsi_dix, dpsi_diy = np.gradient(psi_grid)
-        dix_dr = 1.0 / np.gradient(r)
-        diy_dz = 1.0 / np.gradient(z)
+        dpsi_dix, dpsi_diy = np.gradient(psi_grid, edge_order=2)
+        dix_dr = 1.0 / np.gradient(r, edge_order=2)
+        diy_dz = 1.0 / np.gradient(z, edge_order=2)
 
         dpsi_dr = Interpolate2DCubic(r, z, dpsi_dix * dix_dr[:, np.newaxis])
         dpsi_dz = Interpolate2DCubic(r, z, dpsi_diy * diy_dz[np.newaxis, :])
@@ -129,15 +132,46 @@ cdef class EFITEquilibrium:
         for i, ri in enumerate(r):
             psin[i] = self.psi_normalised(ri, z)
 
-        # filter out all points that lie outside the LCFS
-        # todo: this needs to be more intelligent as in cuts samples that straddle the LCFS
-        # todo: this should really keep going until monotonicity ends (if that occurs inside psin=1.0 then mapping is impossible)
-        # i = psin <= 1.0
-        # psin = psin[i]
-        # r = r[i]
+        # check for monotonicity
+        if (psin[0] < psin[1] and (np.diff(psin) <= 0).any()) or (psin[0] >= psin[1] and (np.diff(psin) >= 0).any()):
+            self.psin_to_r = None
+            return
 
         # interpolate sampled data, allowing a small bit of extrapolation to cope with numerical sampling accuracy
         self.psin_to_r = Interpolate1DCubic(psin, r, extrapolate=True, extrapolation_range=SAMPLE_RESOLUTION, extrapolation_type='quadratic')
+
+    def map2d(self, object profile, double value_outside_lcfs=0.0):
+        """
+        Maps a 1D profile onto the equilibrium to give a 2D profile.
+
+        :param profile: A 1D function or 2xN array.
+        :param value_outside_lcfs: Value returned if point requested outside the LCFS (default=0.0).
+        :return: Function2D object.
+        """
+
+        # convert data to a 1d function if not already a function object
+        if isinstance(profile, Function1D) or callable(profile):
+            profile = autowrap_function1d(profile)
+        else:
+            profile = np.array(profile, np.float64)
+            profile = Interpolate1DCubic(profile[0, :], profile[1, :])
+
+        # map around equilibrium
+        f = IsoMapper2D(self.psi_normalised, profile)
+
+        # mask off values outside the lcfs
+        return Blend2D(Constant2D(value_outside_lcfs), f, self.inside_lcfs)
+
+    def map3d(self, object profile, double value_outside_lcfs=0.0):
+        """
+        Maps a 1D profile onto the equilibrium to give a 3D profile.
+
+        :param profile: A 1D function or Nx2 array.
+        :param value_outside_lcfs: Value returned if point requested outside the LCFS (default=0.0).
+        :return: Function3D object.
+        """
+
+        return AxisymmetricMapper(self.map2d(profile))
 
 
 cdef class EFITLCFSMask(Function2D):
@@ -157,7 +191,10 @@ cdef class EFITLCFSMask(Function2D):
 
     def __init__(self, object lcfs_polygon, object psi_normalised):
 
-        self._lcfs_polygon = PolygonMask2D(lcfs_polygon)
+        # polygon mask requires an Nx2 array and it must be c contiguous
+        # transposing simply swaps the indexing, so need to re-instance
+        poly = np.ascontiguousarray(lcfs_polygon.transpose())
+        self._lcfs_polygon = PolygonMask2D(poly)
         self._psi_normalised = autowrap_function2d(psi_normalised)
 
     cdef double evaluate(self, double r, double z) except? -1e999:
@@ -215,7 +252,7 @@ cdef class EFITMagneticField(VectorFunction2D):
 
         else:
 
-            # note: this is an approximation used my EFIT
+            # note: this is an approximation used by EFIT
             # todo: replace with a more accurate vacuum field calculation (if data available)
             bt = self._b_vacuum_magnitude * self._b_vacuum_radius / r
 
