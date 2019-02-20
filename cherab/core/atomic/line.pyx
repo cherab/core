b9b5d5660da654486326c76b7a15c4517e4cf686
mattngc <matthewncarr@gmail.com>
2019-02-20 11:12:33 +0000

    More minor improvements to atomic rates documentation.

--- cherab/core/atomic/line.pyx
+++ cherab/core/atomic/line.pyx
@@ -18,6 +18,32 @@
 
 
 cdef class Line:
+    """A class fully specifies an observed spectroscopic emission line.
+
+    Note that wavelengths are not arguments to this class. This is because in
+    principle the transition has already been fully specified with the other three
+    arguments. The wavelength is looked up in the wavelength database of the
+    atomic data provider.
+
+    :param Element element: The atomic element/isotope to which this emission line belongs.
+    :param int ionisation: The ionisation state of the element/isotope that emits this line.
+    :param tuple transition: A two element tuple that defines the upper and lower electron
+      configuration states of the transition. For hydrogen-like ions it may be enough to
+      specify the n-levels with integers (e.g. (3,2)). For all other ions the full spectroscopic
+      configuration string should be specified for both states. It is up to the atomic data
+      provider package to define the exact notation.
+
+    .. code-block:: pycon
+
+        >>> from cherab.core.atomic import Line, deuterium, carbon
+        >>>
+        >>> # Specifying the d-alpha and d-gamma balmer lines
+        >>> d_alpha = Line(deuterium, 0, (3, 2))
+        >>> d_beta = Line(deuterium, 0, (4, 2))
+        >>>
+        >>> # Specifying a CIII line at 465nm
+        >>> ciii_465 = Line(carbon, 2, ('2s1 3p1 3P4.0', '2s1 3s1 3S1.0'))
+    """
 
     def __init__(self, Element element, int ionisation, tuple transition):
 
