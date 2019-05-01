0c9360ba1c55277cbae786df563d17370e125036
Lovell, Jack J <Jack.Lovell@ukaea.uk>
2019-05-01 10:53:20 +0100

    Flesh out function to create grid derivative operators

--- cherab/tools/inversions/admt_utils.py
+++ cherab/tools/inversions/admt_utils.py
@@ -5,10 +5,11 @@ This is based on work by L. C. Ingesson: see JET-R(99)08 available at
 http://www.euro-fusionscipub.org/wp-content/uploads/2014/11/JETR99008.pdf
 for details.
 """
+import numpy as np
 
 
-def generate_derivative_operators(voxel_coords, grid_1d_to_2d_map,
-                                  grid_2d_to_1d_map):
+def generate_derivative_operators(voxel_coords, grid_index_1d_to_2d_map,
+                                  grid_index_2d_to_1d_map):
     r"""
     Generate the first and second derivative operators for a regular grid.
 
@@ -20,14 +21,14 @@ def generate_derivative_operators(voxel_coords, grid_1d_to_2d_map,
     :param dict grid_2d_to_1d_map: the inverse mapping from a 2D
     spatially-arranged array of voxels to the 1D array.
 
-    :return tuple operators: a named tuple containing the derivative
+    :return dict operators: a dictionary containing the derivative
     operators: Dij for i, y ∊ (x, y) and Di for i ∊ (x, y).
 
     This function assumes that all voxels are rectilinear, with their
     axes aligned to the coordinate axes. If this is not the case, the
     results will be nonsense.
 
-    The return tuple contains all the first and second derivative
+    The return dict contains all the first and second derivative
     operators:
 
     .. math::
@@ -36,7 +37,180 @@ def generate_derivative_operators(voxel_coords, grid_1d_to_2d_map,
 
     etc.
     """
-    return
+    num_cells = len(voxel_coords)
+    cell_centres = np.mean(voxel_coords, axis=1)
+    # Individual derivative operators
+    Dx = np.zeros((num_cells, num_cells))
+    Dy = np.zeros((num_cells, num_cells))
+    Dxx = np.zeros((num_cells, num_cells))
+    Dxy = np.zeros((num_cells, num_cells))
+    Dyy = np.zeros((num_cells, num_cells))
+    # TODO: for now, we assume all voxels have rectangular cross sections
+    # which are approximately identical. As per Ingesson's notation, we
+    # assume voxels are ordered from top left to bottom right, in column-major
+    # order with each successive voxel in a column below the previous one.
+    # We should try to support voxel grids of different voxel sizes too.
+    dx, dy = np.diff(cell_centres[:2], axis=0).flatten()
+    dx = np.min(abs(dx[dx != 0])).item()
+    dy = -np.min(abs(dy[dy != 0])).item()
+
+    # Note that iy increases as y decreases (cells go from top to bottom),
+    # which is the same as Ingesson's notation in equations 37-41
+    # Use the second version of the second derivative boundary formulae, so
+    # that we only need to consider nearest neighbours
+    for ith_cell in range(num_cells):
+        at_top, at_bottom, at_left, at_right = False, False, False, False
+        n_left, n_right, n_below, n_above = np.nan, np.nan, np.nan, np.nan
+        n_above_left, n_above_right, n_below_left, n_below_right = np.nan, np.nan, np.nan, np.nan
+        # get the 2D mesh coordinates of this cell
+        ix, iy = grid_index_1d_to_2d_map[ith_cell]
+
+        try:
+            n_left = grid_index_2d_to_1d_map[ix - 1, iy]  # neighbour 1, left of n0
+        except KeyError:
+            at_left = True
+        else:
+            Dx[ith_cell, n_left] = -1 / 2
+            Dxx[ith_cell, n_left] = 1
+
+        try:
+            n_below_left = grid_index_2d_to_1d_map[ix - 1, iy + 1]  # neighbour 2, below left of n0
+        except KeyError:
+            pass
+            # KeyError does not necessarily mean bottom AND left
+        else:
+            Dxy[ith_cell, n_below_left] = 1 / 4
+
+        try:
+            n_below = grid_index_2d_to_1d_map[ix, iy + 1]  # neighbour 3, below n0
+        except KeyError:
+            at_bottom = True
+        else:
+            Dy[ith_cell, n_below] = -1 / 2
+            Dyy[ith_cell, n_below] = 1
+
+        try:
+            n_below_right = grid_index_2d_to_1d_map[ix + 1, iy + 1]  # neighbour 4, below right of n0
+        except KeyError:
+            pass
+        else:
+            Dxy[ith_cell, n_below_right] = -1 / 4
+
+        try:
+            n_right = grid_index_2d_to_1d_map[ix + 1, iy]  # neighbour 5, right of n0
+        except KeyError:
+            at_right = True
+        else:
+            Dx[ith_cell, n_right] = 1 / 2
+            Dxx[ith_cell, n_right] = 1
+
+        try:
+            n_above_right = grid_index_2d_to_1d_map[ix + 1, iy - 1]  # neighbour 6, above right of n0
+        except KeyError:
+            pass
+        else:
+            Dxy[ith_cell, n_above_right] = 1 / 4
+
+        try:
+            n_above = grid_index_2d_to_1d_map[ix, iy - 1]  # neighbour 7, above n0
+        except KeyError:
+            at_top = True
+        else:
+            Dy[ith_cell, n_above] = 1 / 2
+            Dyy[ith_cell, n_above] = 1
+
+        try:
+            n_above_left = grid_index_2d_to_1d_map[ix - 1, iy - 1]  # neighbour 8, above left of n0
+        except KeyError:
+            pass
+        else:
+            Dxy[ith_cell, n_above_left] = -1 / 4
+
+        top_left = at_top and at_left
+        top_right = at_top and at_right
+        bottom_left = at_bottom and at_left
+        bottom_right = at_bottom and at_right
+
+        Dxx[ith_cell, ith_cell] = -2
+        Dyy[ith_cell, ith_cell] = -2
+
+        if at_left:
+            Dx[ith_cell, ith_cell] = -1
+            Dx[ith_cell, n_right] = 1
+            Dxx[ith_cell, ith_cell] = -1
+            Dxx[ith_cell, n_right] = 1
+            if not (top_left or bottom_left):
+                Dxy[ith_cell, n_above_right] = 1 / 2
+                Dxy[ith_cell, n_below] = 1 / 2
+                Dxy[ith_cell, n_above] = -1 / 2
+                Dxy[ith_cell, n_below_right] = -1 / 2
+
+        if at_right:
+            Dx[ith_cell, n_left] = -1
+            Dx[ith_cell, ith_cell] = 1
+            Dxx[ith_cell, n_left] = -1
+            Dxx[ith_cell, ith_cell] = 1
+            if not (top_right or bottom_right):
+                Dxy[ith_cell, n_above] = 1 / 2
+                Dxy[ith_cell, n_below_left] = 1 / 2
+                Dxy[ith_cell, n_above_left] = -1 / 2
+                Dxy[ith_cell, n_below] = -1 / 2
+
+        if at_top:
+            Dy[ith_cell, n_below] = -1
+            Dy[ith_cell, ith_cell] = 1
+            Dyy[ith_cell, n_below] = -1
+            Dyy[ith_cell, ith_cell] = 1
+            if not (top_left or top_right):
+                Dxy[ith_cell, n_right] = 1 / 2
+                Dxy[ith_cell, n_below_left] = 1 / 2
+                Dxy[ith_cell, n_left] = -1 / 2
+                Dxy[ith_cell, n_below_right] = -1 / 2
+
+        if at_bottom:
+            Dy[ith_cell, n_above] = 1
+            Dy[ith_cell, ith_cell] = -1
+            Dyy[ith_cell, n_above] = 1
+            Dyy[ith_cell, ith_cell] = -1
+            if not (bottom_left or bottom_right):
+                Dxy[ith_cell, n_above_right] = 1 / 2
+                Dxy[ith_cell, n_left] = 1 / 2
+                Dxy[ith_cell, n_above_left] = -1 / 2
+                Dxy[ith_cell, n_right] = -1 / 2
+
+        if top_left:
+            Dxy[ith_cell, n_below] = 1
+            Dxy[ith_cell, n_right] = 1
+            Dxy[ith_cell, ith_cell] = -1
+            Dxy[ith_cell, n_below_right] = -1
+
+        if top_right:
+            Dxy[ith_cell, ith_cell] = 1
+            Dxy[ith_cell, n_below_left] = 1
+            Dxy[ith_cell, n_left] = -1
+            Dxy[ith_cell, n_below] = -1
+
+        if bottom_left:
+            Dxy[ith_cell, n_above_right] = 1
+            Dxy[ith_cell, ith_cell] = 1
+            Dxy[ith_cell, n_above] = -1
+            Dxy[ith_cell, n_right] = -1
+
+        if bottom_right:
+            Dxy[ith_cell, n_above] = 1
+            Dxy[ith_cell, n_left] = 1
+            Dxy[ith_cell, ith_cell] = -1
+            Dxy[ith_cell, n_above_left] = -1
+
+    Dx = Dx / dx
+    Dy = Dy / dy
+    Dxx = Dxx / dx**2
+    Dyy = Dyy / dy**2
+    Dxy = Dxy / (dx * dy)
+
+    # Package all operators up into a dictionary
+    operators = dict(Dx=Dx, Dy=Dy, Dxx=Dxx, Dyy=Dyy, Dxy=Dxy)
+    return operators
 
 
 def calculate_admt(voxel_radii, derivative_operators, psi_at_voxels, anisotropy=10):
