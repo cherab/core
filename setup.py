63e7205e9d87178056ebf8f3d7ca9d458a2af60f
Dr Alex Meakins <alex.meakins@ccfe.ac.uk>
2019-11-24 15:26:22 +0000

    Updated requirements.

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering :: Physics"
     ],
-    install_requires=['numpy', 'scipy', 'matplotlib', 'raysect>=0.6.0', 'cython>=0.28'],
+    install_requires=['numpy', 'scipy', 'matplotlib', 'raysect==0.6.0', 'cython>=0.28'],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
