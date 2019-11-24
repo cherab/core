11d90e30f276fc1524991e539e62dd9c84fa50f6
Dr Alex Meakins <alex.meakins@ccfe.ac.uk>
2019-11-24 15:36:19 +0000

    Disabled tests that incorrectly requires rate data files. Tests must be stand alone.

--- cherab/openadas/tests/test_adf11_charges.py
+++ cherab/openadas/tests/test_adf11_charges.py
@@ -8,49 +8,50 @@ import unittest
 from cherab.openadas.rates.atomic import IonisationRate, RecombinationRate, ThermalCXRate
 
 
-class TestADAS2CherabChargeConvention(unittest.TestCase):
-
-    def test_ionising_charge(self):
-
-        adas = OpenADAS(permit_extrapolation=False)
-        # todo get a list of all elements and test
-        elem = neon
-        # test if ionisation rates for Z=0 to Z=atomic_number-1 exist as they should
-        for i in range(elem.atomic_number):
-            self.assertIsInstance(adas.ionisation_rate(ion=elem, charge=0), IonisationRate,
-                                  "Ionisation of {}{}+ is missing".format(elem.symbol, i))
-
-        # test if ionisation of bare nucleus is not available
-        self.assertRaises(RuntimeError, adas.ionisation_rate, ion=elem, charge=elem.atomic_number)
-
-    def test_recombining_charge(self):
-
-        adas = OpenADAS(permit_extrapolation=False)
-        # todo get a list of all elements and test
-        elem = neon
-        # test if ionisation rates for Z=1 to Z=atomic_number exist as they should
-        for i in range(1, elem.atomic_number + 1):
-            self.assertIsInstance(adas.recombination_rate(ion=elem, charge=i), RecombinationRate,
-                                  "Recombination of {}{}+ is missing".format(elem.symbol, i))
-
-        # test if ionisation of bare nucleus is not available
-        self.assertRaises(RuntimeError, adas.recombination_rate, ion=elem, charge=0)
-
-    def test_cx_charge(self):
-
-        adas = OpenADAS(permit_extrapolation=False)
-        # todo get a list of all elements and test
-        elem = neon
-        donor = hydrogen
-        # test if thermal cx rates for Z=1 to Z=atomic_number exist as they should
-        for i in range(1, elem.atomic_number + 1):
-            self.assertIsInstance(
-                adas.thermal_cx_rate(donor_element=donor, donor_charge=0, receiver_element=elem, receiver_charge=i),
-                ThermalCXRate, "Recombination of {}{}+ is missing".format(elem.symbol, i))
-
-        # test if ionisation of bare nucleus is not available
-        self.assertRaises(RuntimeError, adas.thermal_cx_rate, donor_element=donor, donor_charge=0,
-                          receiver_element=elem, receiver_charge=0)
+# Todo: this uses rate data, it must be stand alone. Fix it.
+# class TestADAS2CherabChargeConvention(unittest.TestCase):
+#
+#     def test_ionising_charge(self):
+#
+#         adas = OpenADAS(permit_extrapolation=False)
+#         # todo get a list of all elements and test
+#         elem = neon
+#         # test if ionisation rates for Z=0 to Z=atomic_number-1 exist as they should
+#         for i in range(elem.atomic_number):
+#             self.assertIsInstance(adas.ionisation_rate(ion=elem, charge=0), IonisationRate,
+#                                   "Ionisation of {}{}+ is missing".format(elem.symbol, i))
+#
+#         # test if ionisation of bare nucleus is not available
+#         self.assertRaises(RuntimeError, adas.ionisation_rate, ion=elem, charge=elem.atomic_number)
+#
+#     def test_recombining_charge(self):
+#
+#         adas = OpenADAS(permit_extrapolation=False)
+#         # todo get a list of all elements and test
+#         elem = neon
+#         # test if ionisation rates for Z=1 to Z=atomic_number exist as they should
+#         for i in range(1, elem.atomic_number + 1):
+#             self.assertIsInstance(adas.recombination_rate(ion=elem, charge=i), RecombinationRate,
+#                                   "Recombination of {}{}+ is missing".format(elem.symbol, i))
+#
+#         # test if ionisation of bare nucleus is not available
+#         self.assertRaises(RuntimeError, adas.recombination_rate, ion=elem, charge=0)
+#
+#     def test_cx_charge(self):
+#
+#         adas = OpenADAS(permit_extrapolation=False)
+#         # todo get a list of all elements and test
+#         elem = neon
+#         donor = hydrogen
+#         # test if thermal cx rates for Z=1 to Z=atomic_number exist as they should
+#         for i in range(1, elem.atomic_number + 1):
+#             self.assertIsInstance(
+#                 adas.thermal_cx_rate(donor_element=donor, donor_charge=0, receiver_element=elem, receiver_charge=i),
+#                 ThermalCXRate, "Recombination of {}{}+ is missing".format(elem.symbol, i))
+#
+#         # test if ionisation of bare nucleus is not available
+#         self.assertRaises(RuntimeError, adas.thermal_cx_rate, donor_element=donor, donor_charge=0,
+#                           receiver_element=elem, receiver_charge=0)
 
 
 if __name__ == "__main__":
