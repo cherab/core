8a700c943c3866a2b6e58fecb99878277108a384
MatejTomes <tomes@ipp.cas.cz>
2020-01-08 16:57:50 +0100

    Change logic of laserspectrum class
    Laser spectrum_base is a Function1D which caches values for wavelengths defined by min_wavelength, max_wavelength, bins. Central_wavelength property removed from the class.

--- cherab/core/laser/models/laserspectrum_base.pxd
+++ cherab/core/laser/models/laserspectrum_base.pxd
@@ -1,19 +1,24 @@
 
 from raysect.core.math.function cimport Function1D
 
-import numpy as np
-cimport numpy as np
+from cherab.core.laser.node cimport Laser
+from cherab.core.utility.constants cimport SPEED_OF_LIGHT, PLANCK_CONSTANT
+from numpy cimport ndarray
+
+
+cdef double CONSTANT_HC = SPEED_OF_LIGHT * PLANCK_CONSTANT
 
 cdef class LaserSpectrum_base(Function1D):
 
     cdef:
-        double _min_wavelength, _max_wavelength, _central_wavelength
-        int bins
-        np.ndarray _radiance, _photons, _wavelengths
-        cdef np.float_t [:] _radiance_mv, _photons_mv, _wavelngths_mv
+        double _min_wavelength, _max_wavelength, _delta_wavelength
+        int _bins
+        ndarray _power, _psd, _photons, _wavelengths # psd: power spectral density [w/nm]
+        double[::1] _power_mv, _psd_mv, _photons_mv, _wavelengths_mv 
+        object _notifier
         
-    cpdef void _create_cache_spectrum_mv(sefl)
-
     cpdef double evaluate_integral(self, double lower_limit, double upper_limit)
 
-    cpdef void _create_cache_spectrum_mv(sefl)
+    cpdef void _update_cache(self)
+
+    cdef double _photon_energy(self, double wavelength)
--- cherab/core/laser/models/laserspectrum_base.pyx
+++ cherab/core/laser/models/laserspectrum_base.pyx
@@ -3,31 +3,28 @@ from raysect.core.math.function cimport Function1D
 from raysect.optical cimport Point3D, Vector3D
 
 from cherab.core.utility import Notifier
-from cherab.core.laser.scattering cimport ScatteringModel
-
+from cherab.core.laser.node import Laser
 import numpy as np
 cimport numpy as np
 
+from cherab.core.utility.constants cimport SPEED_OF_LIGHT, PLANCK_CONSTANT
+cdef double CONSTANT_HC = SPEED_OF_LIGHT * PLANCK_CONSTANT
+
 cdef class LaserSpectrum_base(Function1D):
 
-    def __init__(self, min_wavelength, max_wavelength, bins, central_wavelength):
+    def __init__(self, double min_wavelength, double max_wavelength, int bins):
 
         super().__init__()
-        self._check_wavelength_validity(min_wavelength, max_wavelength, central_wavelength)
 
-        self.min_wavelength = min_wavelength
-        self.max_wavelength = max_wavelength
-        self.central_wavelength = central_wavelength
+        self._notifier = Notifier()
 
-    @property
-    def central_wavelength(self):
-        return self._central_wavelength
+        self._check_wavelength_validity(min_wavelength, max_wavelength)
 
-    @central_wavelength.setter
-    def central_wavelength(self, double value):
+        self._min_wavelength = min_wavelength
+        self._max_wavelength = max_wavelength
 
-        self._central_wavelength = value
-        self._check_wavelength_validity()
+        self.bins = bins
+
 
     @property
     def min_wavelength(self):
@@ -37,7 +34,7 @@ cdef class LaserSpectrum_base(Function1D):
     def min_wavelength(self, double value):
 
         self._min_wavelength = value
-        self._check_wavelength_validity()
+        self._update_cache()
 
     @property
     def max_wavelength(self):
@@ -47,32 +44,81 @@ cdef class LaserSpectrum_base(Function1D):
     def max_wavelength(self, double value):
 
         self._max_wavelength = value
-        self._check_wavelength_validity()
+        self._update_cache()
 
-    def _check_wavelength_validity(self, min_wavelength = None, max_wavelength = None, central_wavelength = None):
+    @property
+    def bins(self):
+        return self._bins
+
+    @bins.setter
+    def bins(self, int value):
+        if not value > 0:
+            raise ValueError("Value has to be larger than 0")
+
+        self._bins = value
+        self._update_cache()
+
+    @property
+    def wavelengths(self):
+        return self._wavelengths
+
+    @property
+    def psd(self):
+        return self._psd
+
+    @property
+    def photons(self):
+        return self._photons
+
+    @property
+    def delta_wavelength(self):
+        return self._delta_wavelength
+
+    def _check_wavelength_validity(self, min_wavelength = None, max_wavelength = None):
 
         if min_wavelength is None:
             min_wavelength = self._min_wavelength
         if max_wavelength is None:
             max_wavelength = self._max_wavelength
-        if central_wavelength is None:
-            central_wavelength = self._central_wavelength
-        
+
         if not min_wavelength > 0:
             raise ValueError("min_wavelength has to be larger than 0, but {} passed.".format(min_wavelength))
         if not max_wavelength > 0:
             raise ValueError("min_wavelength has to be larger than 0, but {} passed.".format(max_wavelength))
-        if not central_wavelength > 0:
-            raise ValueError("min_wavelength has to be larger than 0, but {} passed.".format(central_wavelength))
 
-        if min_wavelength > central_wavelength:
-            raise ValueError("Central wavelength has to be larger than Min_wavelength: min_wavelength={} > central_wavelength={}".format(min_wavelength, central_wavelength))
-        if max_wavelength < central_wavelength:
-            raise ValueError("Central wavelength has to be smaller than max_wavelength: max_wavelength={} < central_wavelength={}".format(min_wavelength, central_wavelength))
+        if min_wavelength > max_wavelength:
+            raise ValueError("min_wavelength has to be smaller than max_wavelength: min_wavelength={} > max_wavelength={}".format(min_wavelength, max_wavelength))
     
-    cpdef void _create_cache_spectrum_mv(self):
+    cpdef void _update_cache(self):
 
-        raise NotImplementedError('Virtual method must be implemented in a sub-class.')
+        cdef:
+            Py_ssize_t index
+
+        self._delta_wavelength = (self._max_wavelength - self._min_wavelength) / self._bins
+        self._wavelengths = np.zeros((self.bins), dtype=np.double)
+        self._wavelengths_mv = self._wavelengths
+        
+        for index in range(self._bins):
+            self._wavelengths[index] = self._min_wavelength + (0.5 + index) * self._delta_wavelength
+
+        self._psd = np.zeros((self._bins), dtype=np.double)
+        self._power_mv = np.zeros((self._bins), dtype=np.double)
+        
+        self._psd_mv = self._psd
+
+        self._photons = np.zeros((self._bins), dtype=np.double)
+        self._photons_mv = self._photons  
+
+        for index in range(self._bins):
+            self._power_mv[index] = self.evaluate(self._wavelengths_mv[index])
+            self._psd_mv[index] = self._power_mv[index] / self._delta_wavelength
+            self._photons_mv[index] = self._psd_mv[index] / self._photon_energy(self._wavelengths_mv[index])
+
+        #notify about changes
+        self._notifier.notify()
+
+    cdef double _photon_energy(self, double wavelength):
+        return CONSTANT_HC / (wavelength * 1e-9)
 
     cpdef double evaluate_integral(self, double lower_limit, double upper_limit):
         raise NotImplementedError('Virtual method must be implemented in a sub-class.')
