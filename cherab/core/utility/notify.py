33f62860df8e6878fd4ac0b9300ad766e2ece92f
Dr Alex Meakins <CnlPepper@users.noreply.github.com>
2020-05-19 15:33:23 +0100

    Modified _is_present_method to use __name__ rather than __func__. Fixes #221.

--- cherab/core/utility/notify.py
+++ cherab/core/utility/notify.py
@@ -122,7 +122,9 @@ class Notifier:
 
         for reference in self._callbacks_refs:
             if isinstance(reference, tuple):
-                if reference[0]() == callback.__self__ and reference[1] == callback.__name__:
+                instance = reference[0]()
+                method = reference[1]                
+                if callback.__self__ == instance and callback.__name__ == method:
                     self._purge([reference])
                     break
 
@@ -145,7 +147,7 @@ class Notifier:
             if isinstance(reference, tuple):
                 instance = reference[0]()
                 method = reference[1]
-                if callback.__self__ is instance and callback.__func__ is method:
+                if callback.__self__ == instance and callback.__name__ == method:
                     return True
 
         return False
