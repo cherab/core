a7caca2e0f2593017c7085a32bd8446c7c10b977
Matej Tomes <tomes@ipp.cas.cz>
2022-12-22 17:42:05 +0100

    Rewritten TS model with segmented laser geometry (#219)
    I decided to do new PR, because at the end there were many changes:
    
        laser geometry is now segmented cylinder as suggested by @mattngc. This propagated into handling of information between plasma, laser node, laser model and scattering model. This was neccessary to simplify handling of notifications, deletions and etc.
            laser model and spectrum hold no plasma and laser reference. Their methods return requested properties (e.g. laser power) in laser node frame.
            Laser material now obtains all the information from plasma, laser spectrum and laser model needed by the scattering model.
            Scattering model works in the frame of reference of the laser node. Method for scattered spectrum calcullation must now get all the neccessary plasma and laser variables (e.g. Te, ne, directions, etc.)
        laser models were rewritten and new added (uniform, bivariate and trivariate normal spatial distributions, Gaussian beam model). Lasers are now described by pulse energy and pulse temporal lengh, this is transformed to spatial power densities using given distributions.
        laser spectrum power spectral density was corrected.
    
    Unittests checking the following were added:
    
        laser node
            initialisation
            propagation of notifications between laser model, laser spectrum, scattering model, laser geometry and laser material
            position of the laser segments compared to the expected one
        laser spectrum
            initialisation and changes
            normalisation of models
        laser models
            initialisation
            polarisation direction
            correct values of power for spatial points
            integration of laser power over pulse spatial extent is compared to expected pulse energy
        scattering model
            scaterred spectrum traced by a ray is compared to expected values calculated by semi-analytical approach.

---
+++ cherab/core/model/laser/__init__.py
@@ -0,0 +1,4 @@
+from .laserspectrum import ConstantSpectrum, GaussianSpectrum
+from .profile import UniformEnergyDensity, ConstantAxisymmetricGaussian
+from .profile import ConstantBivariateGaussian, TrivariateGaussian, GaussianBeamAxisymmetric
+from .model import SeldenMatobaThomsonSpectrum
